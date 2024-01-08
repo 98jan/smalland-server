3285e72f8aee12c04158ec03aff2e75d2b176437
Jan <46779261+98jan@users.noreply.github.com>
2024-01-08 22:38:07 +0100

    create initial folders

--- setup.sh
+++ setup.sh
@@ -5,6 +5,18 @@ if [[ "${SKIPUPDATE,,}" != "false" ]] && [ ! -f "/config/gamefiles/FactoryServer
     SKIPUPDATE="false"
 fi
 
+# create needed folders
+mkdir -p \
+  /config/backups \
+  /config/gamefiles \
+  /config/overrides \
+  /config/saved/blueprints \
+  /config/saved/server \
+  /config/LinuxServer \
+  /config/Logs \
+  /config/server \
+  || exit 1
+
 if [[ "${SKIPUPDATE,,}" != "true" ]]; then
   printf "Downloading the latest version of the game...\\n"
   steamcmd +force_install_dir /config/gamefiles +login anonymous +app_update "$STEAMAPPID" -beta "$STEAMBETAFLAG" validate +quit
